addd33f5e3ada146b67f93f7765dcce20dfe5037
Kim Turboutique <127644189+KimTurboutique@users.noreply.github.com>
2023-06-24 15:29:44 -0400

    added api routes

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # Note-pad
+
+https://www.youtube.com/watch?v=SccSCuHhOw0
--- server.js
+++ server.js
@@ -1,5 +1,8 @@
 const express = require('express');
+const fs = require('fs');
+
 const htmlRoutes = require('./routes/htmlRoutes');
+const apiRoutes = require('./routes/apiRoutes');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
@@ -9,5 +12,5 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.static('public'));
 
 app.use('/', htmlRoutes);
-
+app.use('/api', apiRoutes);
 app.listen(PORT, () => console.log(`Server listening on PORT: ${PORT}`))
